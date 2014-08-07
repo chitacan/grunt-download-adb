243d0e6eed1997341b8fd0958b0d6e933e8881f6
kyung yeol kim <chitacan@gmail.com>
2014-08-08 00:40:46 +0900

    add some constants.

--- tasks/download-adb-task.js
+++ tasks/download-adb-task.js
@@ -1,6 +1,13 @@
 'use strict'
 
 module.exports = function(grunt) {
+  var URL  = 'http://dl-ssl.google.com/android/repository/'
+    , REPO = 'repository-8.xml'
+
+  var LINUX = 'linux'
+    , WIN   = 'windows'
+    , OSX   = 'macosx'
+
   grunt.registerTask('download-adb', 'Download adb', function() {
     grunt.log.writeln('Download adb');
   });
